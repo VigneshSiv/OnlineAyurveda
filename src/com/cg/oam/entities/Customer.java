b7aab32b1f4d2e24adfcc783cd03d39c740f5caa
Jackson R <88396971+jackrz@users.noreply.github.com>
2021-08-06 17:09:51 +0530

    Add files via upload

---
+++ src/com/cg/oam/entities/Customer.java
@@ -0,0 +1,11 @@
+package com.cg.oam.entities;
+
+public class Customer {
+
+	private int customerId;
+	private String customerName;
+	private String customerPassword;
+	private Long mobileNumber;
+	private String emailId;
+	
+}
