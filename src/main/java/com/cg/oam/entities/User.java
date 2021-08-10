e5610f1640983278fed2548d0f79c48b30404a61
VigneshSiv <88703277+VigneshSiv@users.noreply.github.com>
2021-08-10 11:57:16 +0530

    User.java

--- src/main/java/com/cg/oam/entities/User.java
+++ src/main/java/com/cg/oam/entities/User.java
@@ -1,9 +1,67 @@
 package com.cg.oam.entities;
 
-public class User {
+import javax.persistence.*;
 
+
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@Column(name="USER_ID")
 	private int userId;
+	@Column(name="USER_NAME")
 	private String userName;
+	@Column(name="USER_TYPE")
 	private String userType;
+	
+	
+	
+	
+	
+	public User() {
+		
+	}
+
+
+	public User(int userId, String userName, String userType) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.userType = userType;
+	}
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public String getUserType() {
+		return userType;
+	}
+
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	
+	
+	
+	
 	
 }
