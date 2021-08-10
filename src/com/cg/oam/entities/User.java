bfb32963ae86e9e3ada4ca0eb923c1ce26c10b37
Jackson R <88396971+jackrz@users.noreply.github.com>
2021-08-10 08:54:05 +0530

    Update User.java
    Changes made to User and updated getter and setter methods

--- src/com/cg/oam/entities/User.java
+++ src/com/cg/oam/entities/User.java
@@ -6,4 +6,35 @@ public class User {
 	private String userName;
 	private String userType;
 	
+	public User(int userId, String userName, String userType) {
+		this.userId = userId;
+		this.userName = userName;
+		this.userType = userType;
+		
+	}
+// Getter and Setter
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	
 }
